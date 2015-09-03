624b8526d1a6538f2f5bb0f35486cf6db179da1e
Chris M. Balz <christopherbalz@yahoo.com>
2015-09-02 19:00:53 -0700

    work-in-progress

---
+++ src/cljs/vibrashank/history.cljs
@@ -0,0 +1,15 @@
+(ns vibrashank.core
+  (:require
+   [secretary.core :as secretary :include-macros true]
+   [goog.events :as events]
+   [goog.history.EventType :as EventType]
+   [vibrashank.routes]))
+
+;; This must be called after routes have been defined
+(defn hook-browser-navigation! []
+  (doto (History.)
+    (events/listen
+     EventType/NAVIGATE
+     (fn [event]
+       (secretary/dispatch! (.-token event))))
+    (.setEnabled true)))
---
+++ src/cljs/vibrashank/views.cljs
@@ -0,0 +1,15 @@
+(ns vibrashank.core
+    (:require [reagent.core :as reagent :refer [atom]]
+              [reagent.session :as session]))
+
+(defn home-page []
+  [:div [:h2 "Welcome to vibrashank"]
+   [:div [:a {:href "#/about"} "go to about page"]]])
+
+(defn about-page []
+  [:div [:h2 "About vibrashank"]
+   [:section [:a {:href "https://github.com/christopherbalz/vibrashank"} "Project Home"]]
+   [:div [:a {:href "#/"} "go to the home page"]]])
+
+(defn current-page []
+  [:div [(session/get :current-page)]])
