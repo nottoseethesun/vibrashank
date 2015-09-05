6eebe45df1ec46301c6853a778bb516a2f95ecce
Chris M. Balz <christopherbalz@yahoo.com>
2015-09-04 20:06:08 -0700

    Added newfound love, freactive.core.  Also adding core.async.

--- project.clj
+++ project.clj
@@ -17,7 +17,9 @@
                  [hiccup "1.0.5"]
                  [environ "1.0.0"]
                  [org.clojure/clojurescript "1.7.107" :scope "provided"]
-                 [secretary "1.2.3"]]
+                 [secretary "1.2.3"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [freactive.core "0.2.0-alpha1"]]  ;; freactive.core supplies the cursor Vibrashank uses.
 
   :plugins [[lein-environ "1.0.0"]
             [lein-asset-minifier "0.2.2"]]
